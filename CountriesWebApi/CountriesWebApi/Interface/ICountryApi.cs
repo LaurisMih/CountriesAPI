403e0864b8e0b6ed2325b9cadbadbe48cca0f172
LaurisMih <laurisroki2@gmail.com>
2022-12-01 21:57:36 +0300

    update

---
+++ CountriesWebApi/CountriesWebApi/Interface/ICountryApi.cs
@@ -0,0 +1,14 @@
+﻿using CountriesWebApi.Models;
+using Refit;
+
+namespace CountriesWebApi.Interface
+{
+    public interface ICountryApi
+    {
+        [Get("/v2/regionalbloc/eu")]
+        Task<List<Country>> GetCountries();
+
+       /* [Get("/v2/name/{name}")]
+        Task<List<Country>> GetCountriesByName(string countryName);*/
+    }
+}
---
+++ CountriesWebApi/CountriesWebApi/Program.cs
@@ -0,0 +1,38 @@
+using CountriesWebApi.Interface;
+using Refit;
+
+// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+builder.Services.AddRefitClient<ICountryApi>().ConfigureHttpClient(c =>
+{
+    c.BaseAddress = new Uri(configuration["ICountryApi:BaseAddress"]);
+}
+);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
